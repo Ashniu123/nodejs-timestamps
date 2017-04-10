be3f6af7ca2a1fd50722d8f61466cde69f21e282
ashniu123 <nisheet1.sinvhal@gmail.com>
2017-04-10 22:19:27 +0530

    Working timestamp conversions

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var moment=require('moment');
+var app = express();
+
+app.get('/:time', function (req, res) {
+    var time=req.params.time,json={};
+    console.log(time);
+    if(time.match(/^\d+$/g)){
+        console.log("unix timestamp");
+        json["unix"]=time;
+        json["natural"]=moment.unix(time).format("MMMM D, YYYY");
+    }else{
+        console.log("natural timestamp");
+        time=moment(time,"MMMM D, YYYY");
+        json["unix"]=time.format("X");
+        json["natural"]=time.format("MMMM D, YYYY");
+    }
+    res.status(200).end(JSON.stringify(json));
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
